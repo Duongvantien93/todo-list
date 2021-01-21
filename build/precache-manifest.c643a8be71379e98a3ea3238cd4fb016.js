5865a5f5d8ac1dc0048020ae8baaa877be4340c6
Duong Van Tien <zai.ha.tay@gmail.com>
2021-01-21 22:47:21 +0700

    update engines

---
+++ build/precache-manifest.c643a8be71379e98a3ea3238cd4fb016.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a329e461200c670a8a2a3ad28e40db3f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "54e120ebe28caebb452f",
+    "url": "/static/css/2.59564cdb.chunk.css"
+  },
+  {
+    "revision": "e2042501b79bac266a92",
+    "url": "/static/css/main.2321910c.chunk.css"
+  },
+  {
+    "revision": "54e120ebe28caebb452f",
+    "url": "/static/js/2.ccee5c60.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.ccee5c60.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e2042501b79bac266a92",
+    "url": "/static/js/main.bc2e2a2a.chunk.js"
+  },
+  {
+    "revision": "354fc84318908be3e968",
+    "url": "/static/js/runtime-main.f5890048.js"
+  },
+  {
+    "revision": "dff9a821eace3390bcb63f952d18ffbb",
+    "url": "/static/media/icon-close.dff9a821.svg"
+  },
+  {
+    "revision": "a097375bb5439e6a1b1a7827fde7fb8c",
+    "url": "/static/media/icon-edit.a097375b.svg"
+  },
+  {
+    "revision": "a89abe84a62da0fdf7823e260a21eb37",
+    "url": "/static/media/icon-face-sad.a89abe84.svg"
+  },
+  {
+    "revision": "9db65898dfeaaa65bb4a645f28a1745a",
+    "url": "/static/media/icon-home.9db65898.svg"
+  },
+  {
+    "revision": "e884b90e5218869940e3b3de82a88c73",
+    "url": "/static/media/icon-people.e884b90e.svg"
+  },
+  {
+    "revision": "5f1f1fe06895f0c62cb042ee494c2a54",
+    "url": "/static/media/icon-phone.5f1f1fe0.svg"
+  },
+  {
+    "revision": "1565a539179aeda8f3eeeb7cb1c46d81",
+    "url": "/static/media/icon-trash.1565a539.svg"
+  },
+  {
+    "revision": "502bbf3fa953730a314060e654327aeb",
+    "url": "/static/media/icon-user-plus.502bbf3f.svg"
+  },
+  {
+    "revision": "cadc7e983568c802aa62b9ef152d3191",
+    "url": "/static/media/icon-user.cadc7e98.svg"
+  }
+]);
